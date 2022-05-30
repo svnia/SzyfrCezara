d85427afd69d9b7c09dfb3e96eb20a77a70be214
svnia <100943529+svnia@users.noreply.github.com>
2022-05-30 15:05:00 +0200

    Update Program.cs

--- SzyfrCezara/Program.cs
+++ SzyfrCezara/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+string alfabet = "ABCDEFGHIJKLMNOPRSTUWYZ";
+Console.WriteLine("Wartosc ile ma sie przesunac:");
 
+int szyfr;
+Int32.TryParse(Console.ReadLine(), out szyfr);
+
+Console.WriteLine("Podaj Tekst do zaszyfrowania");
+string tekst = Console.ReadLine().Replace(" ", "");
+tekst = tekst.ToUpper();
+string wyjscie = "";
+for (int i = 0; i < tekst.Length; i++)
+{
+    alfabet = "ABCDEFGHIJKLMNOPRSTUWYZ";
+    var poszyfr = (alfabet.IndexOf(tekst[i]) + szyfr) % alfabet.Length;
+    wyjscie += alfabet[poszyfr].ToString();
+}
+Console.WriteLine(wyjscie);
